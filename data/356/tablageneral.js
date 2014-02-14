3e8761a3b252d86e9d9739d55f7a2b62785d768e
Miguel Mendoza <miguel.mendoza@esmas.net>
2014-02-13 18:05:31 -0600

    Create tablageneral.js

---
+++ data/356/tablageneral.js
@@ -0,0 +1,133 @@
+teamsClassification(
+    {
+        "idTorneo": "356",
+        "nameTorneo": "Liga Bancomer MX Clausura 2014",
+        "nameWebTorneo": "copa-mx-clausura-2014",
+        "idJornada": "J7",
+        "nameJornada": "Jornada 7",
+        "dataEstadistica": [
+            {
+                "position": "1",
+                "idTeam": "4",
+                "nameTeam": "Cruz Azul",
+                "webNameTeam": "cruz-azul",
+                "aliasTeam": "CA",
+                "urlLogoClub": "http://lorempixel.com/24/24/",
+                "dataEstadisticas": {
+                    "JJ": "6",
+                    "JG": "5",
+                    "JE": "1",
+                    "JP": "0",
+                    "GF": "13",
+                    "GC": "2",
+                    "DIF": "11",
+                    "PTS": "16"
+                }
+            },{
+                "position": "2",
+                "idTeam": "5",
+                "nameTeam": "Toluca",
+                "webNameTeam": "toluca",
+                "aliasTeam": "TOL",
+                "urlLogoClub": "http://lorempixel.com/24/24/",
+                "dataEstadisticas": { "JJ": "6", "JG": "4", "JE": "1", "JP": "1", "GF": "9", "GC": "4", "DIF": "5", "PTS": "13" }
+            },{
+                "position": "3",
+                "idTeam": "6",
+                "nameTeam": "Ame&#769;rica",
+                "webNameTeam": "america",
+                "aliasTeam": "AME",
+                "urlLogoClub": "http://lorempixel.com/24/24/",
+                "dataEstadisticas": { "JJ": "6", "JG": "4", "JE": "0", "JP": "2", "GF": "7", "GC": "3", "DIF": "4", "PTS": "12" }
+            },{
+                "position": "4",
+                "idTeam": "7",
+                "nameTeam": "Pumas",
+                "webNameTeam": "pumas",
+                "aliasTeam": "PUM",
+                "urlLogoClub": "http://lorempixel.com/24/24/",
+                "dataEstadisticas": { "JJ": "6", "JG": "3", "JE": "1", "JP": "2", "GF": "9", "GC": "7", "DIF": "2", "PTS": "10" }
+            },{
+                "position": "5",
+                "idTeam": "8",
+                "nameTeam": "Pachuca",
+                "webNameTeam": "pachuca",
+                "aliasTeam": "PAC",
+                "urlLogoClub": "http://lorempixel.com/24/24/", 
+                "dataEstadisticas": { "JJ": "6", "JG": "3", "JE": "1", "JP": "2", "GF": "4", "GC": "2", "DIF": "2", "PTS": "10" } 
+            },{
+                "position": "6",
+                "idTeam": "9",
+                "nameTeam": "Quere&#769;taro FC",
+                "webNameTeam": "queretaro",
+                "aliasTeam": "QUE",
+                "urlLogoClub": "http://lorempixel.com/24/24/",
+                "dataEstadisticas": { "JJ": "6", "JG": "3", "JE": "1", "JP": "2", "GF": "5", "GC": "4", "DIF": "1", "PTS": "10" } 
+            },{
+                "position": "7",
+                "idTeam": "10",
+                "nameTeam": "Tijuana",
+                "webNameTeam": "tijuana",
+                "aliasTeam": "TIJ",
+                "urlLogoClub": "http://lorempixel.com/24/24/",
+                "dataEstadisticas": { "JJ": "6", "JG": "3", "JE": "1", "JP": "2", "GF": "5", "GC": "6", "DIF": "-1", "PTS": "10" } 
+            },{
+                "position": "8",
+                "idTeam": "11",
+                "nameTeam": "Guadalajara",
+                "webNameTeam": "guadalajara",
+                "aliasTeam": "GUA",
+                "urlLogoClub": "http://lorempixel.com/24/24/",
+                "dataEstadisticas": { "JJ": "6", "JG": "2", "JE": "3", "JP": "1", "GF": "5", "GC": "5", "DIF": "0", "PTS": "9" } 
+            },{
+                "position": "9",
+                "idTeam": "12",
+                "nameTeam": "Santos",
+                "webNameTeam": "santos",
+                "aliasTeam": "SAN",
+                "urlLogoClub": "http://lorempixel.com/24/24/",
+                "dataEstadisticas": { "JJ": "6", "JG": "1", "JE": "4", "JP": "1", "GF": "5", "GC": "4", "DIF": "1", "PTS": "7" } 
+            },{
+                "position": "10",
+                "idTeam": "13",
+                "nameTeam": "Chiapas FC",
+                "webNameTeam": "chiapas",
+                "aliasTeam": "CHI",
+                "urlLogoClub": "http://lorempixel.com/24/24/",
+                "dataEstadisticas": { "JJ": "6", "JG": "2", "JE": "1", "JP": "3", "GF": "5", "GC": "7", "DIF": "-2", "PTS": "7" } 
+            },{
+                "position": "11",
+                "idTeam": "14",
+                "nameTeam": "Morelia",
+                "webNameTeam": "morelia",
+                "aliasTeam": "MAN",
+                "urlLogoClub": "http://lorempixel.com/24/24/",
+                "dataEstadisticas": { "JJ": "6", "JG": "1", "JE": "3", "JP": "2", "GF": "5", "GC": "5", "DIF": "0", "PTS": "6" } 
+            },{
+                "position": "12",
+                "idTeam": "15",
+                "nameTeam": "Puebla",
+                "webNameTeam": "puebla",
+                "aliasTeam": "PUE",
+                "urlLogoClub": "http://lorempixel.com/24/24/",
+                "dataEstadisticas": { "JJ": "6", "JG": "1", "JE": "3", "JP": "2", "GF": "6", "GC": "7", "DIF": "-1", "PTS": "6" } 
+            },{
+                "position": "13",
+                "idTeam": "16",
+                "nameTeam": "Monterrey",
+                "webNameTeam": "monterrey",
+                "aliasTeam": "MON",
+                "urlLogoClub": "http://lorempixel.com/24/24/",
+                "dataEstadisticas": { "JJ": "6", "JG": "1", "JE": "3", "JP": "2", "GF": "5", "GC": "6", "DIF": "-1", "PTS": "6" } 
+            },{
+                "position": "14",
+                "idTeam": "18",
+                "nameTeam": "Leo&#769;n",
+                "webNameTeam": "leon",
+                "aliasTeam": "LEO",
+                "urlLogoClub": "http://lorempixel.com/24/24/",
+                "dataEstadisticas": { "JJ": "6", "JG": "1", "JE": "2", "JP": "3", "GF": "6", "GC": "8", "DIF": "-2", "PTS": "6" } 
+            }
+        ] 
+    }
+);
