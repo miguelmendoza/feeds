e13c3ff810133b5fda32490c7650516710599809
Miguel Mendoza <miguel.mendoza@esmas.net>
2014-02-13 18:05:59 -0600

    Create tablafase_16.js

---
+++ data/356/tablafase_16.js
@@ -0,0 +1,29 @@
+phasesbytorneo({
+    "idTorneo": "356",
+    "nameTorneo": "Torneo MX Clausura 2014",
+    "nameWebTorneo": "torneo-mx-clausura-2014",
+    "dataFases": [
+        {
+            "nombreFase": "Fase 2",
+            "idFase": "16",
+            "dataJornadas": [
+                {
+                    "idTeam": "4",
+                    "positionTeam": "1"
+                }, 
+                {
+                    "idTeam": "9",
+                    "positionTeam": "2"
+                },
+                {
+                    "idTeam": "7",
+                    "positionTeam": "3"
+                },
+                {
+                    "idTeam": "12",
+                    "positionTeam": "4"
+                }
+            ]
+        }
+    ]
+})
