572e5b5db5da19724db3125d714f71e0c4c7ad29
Miguel Mendoza <miguel.mendoza@esmas.net>
2014-02-13 18:01:32 -0600

    Grupos 1 result

--- wdg_smex_groups_01/js/wdg_smex_groups.js
+++ wdg_smex_groups_01/js/wdg_smex_groups.js
@@ -4,12 +4,18 @@
     var objSettings = false;
     var domain = false;
     var jsonpCallback = false;
+    var arrItems = new Array();
     
     $.fn.gruposTeams = function( params ) {
 
         objGlobal = $.extend( {
             idTorneo: false,
-            idFase: false
+            idFase: false,
+            lblJJ: "JJ",
+            lblJG: "JG",
+            lblJE: "JE",
+            lblJP: "JP",
+            lblPTS: "PTS",
         }, params);
 
         if ( !objGlobal.idTorneo ) {
@@ -26,7 +32,7 @@
 
     };
     
-    function token() {
+    var token = function () {
         
         $objGlobal.html(objSettings.loading);
 
@@ -41,51 +47,88 @@
             success: successData,
             error: function(xhr, ajaxOptions, thrownError) {
                 $objGlobal.html('');
-                console.log( thrownError );
             }
         });
     };
     
-    function createHTML ( data ) {
-        var html = '';
+    var token2 = function () {
+        
+        $objGlobal.html(objSettings.loading);
 
+        $.ajax({
+            type: 'GET',
+            url: 'http://feeds-televisadeportes.dev/data/'+domain,
+            async: false,
+            jsonpCallback: jsonpCallback,
+            contentType: "application/json",
+            dataType: 'jsonp',
+            cache: false,
+            success: function(data){
+                arrItems.push(data);
+            },
+            error: function(xhr, ajaxOptions, thrownError) {
+                $objGlobal.html('');
+            }
+        });
+    };
+    
+    var createHTML = function ( data ) {
+        var html = '';
+        var idTorneo = "";
+        
+        for( var i=0 ; i<data.dataFases.length ; i++ )  {
+            idTorneo = data.idTorneo;
+            domain = idTorneo+'/tablageneral.js';
+            jsonpCallback = 'teamsClassification';
+            token2();
+        }
+        
         html += '<div class="wdg_smex_groups_01" data-enhance="false" style="display:none">';
           html += '<div class="scroll">';
             
             for( var i=0 ; i<data.dataFases.length ; i++ )  {
-              html += '<div class="titulo_goal textcolor-title4">Hexagonal</div>';
+            
+              html += '<div class="titulo_goal textcolor-title4">'+data.dataFases[i].nombreFase+'</div>';
               html += '<div class="goal">';
                 html += '<ul class="head_team textcolor-title4">';
-                  html += '<li>JJ</li>';
-                  html += '<li>JG</li>';
-                  html += '<li>JE</li>';
-                  html += '<li>JP</li>';
-                  html += '<li>PTS</li>';
+                  html += '<li>'+objGlobal.lblJJ+'</li>';
+                  html += '<li>'+objGlobal.lblJG+'</li>';
+                  html += '<li>'+objGlobal.lblJE+'</li>';
+                  html += '<li>'+objGlobal.lblJP+'</li>';
+                  html += '<li>'+objGlobal.lblPTS+'</li>';
                 html += '</ul>';
                 html += '<div class="team_divisor">';
                   html += '<ul class="team">';
-                    for( var j=0 ; j<data.dataFases[i].dataJornadas.length ; j++ )  {
-                      html += '<li class="first_child dotted-right"><p>Panama</p></li>';
-                      html += '<li class="textcolor-title4 dotted-right"><p>6</p></li>';
-                      html += '<li class="textcolor-title4 dotted-right"><p>6</p></li>';
-                      html += '<li class="textcolor-title4 dotted-right"><p>6</p></li>';
-                      html += '<li class="textcolor-title4 dotted-right"><p>6</p></li>';
-                      html += '<li class="last_child textcolor-title4"><p>6</p></li>';
-                    }
+                    
+                        for( var k=0 ; k < data.dataFases[i].dataJornadas.length ; k++ )
+                        {
+                            for( var j=0 ; j < arrItems[0].dataEstadistica.length ; j++ )
+                            {
+                                if( data.dataFases[i].dataJornadas[k].idTeam == arrItems[0].dataEstadistica[j].idTeam )
+                                {
+                                    console.log( arrItems[0].dataEstadistica[j] );
+                                    html += '<li class="first_child dotted-right"><p>'+arrItems[0].dataEstadistica[j].nameTeam+'</p></li>';
+                                    html += '<li class="textcolor-title4 dotted-right"><p>'+arrItems[0].dataEstadistica[j].dataEstadisticas.JJ+'</p></li>';
+                                    html += '<li class="textcolor-title4 dotted-right"><p>'+arrItems[0].dataEstadistica[j].dataEstadisticas.JG+'</p></li>';
+                                    html += '<li class="textcolor-title4 dotted-right"><p>'+arrItems[0].dataEstadistica[j].dataEstadisticas.JE+'</p></li>';
+                                    html += '<li class="textcolor-title4 dotted-right"><p>'+arrItems[0].dataEstadistica[j].dataEstadisticas.JP+'</p></li>';
+                                    html += '<li class="last_child textcolor-title4"><p>'+arrItems[0].dataEstadistica[j].dataEstadisticas.PTS+'</p></li>';
+                                }
+                            }
+                        }
                   html += '</ul>';
                 html += '</div>';
               html += '</div>';
+            
             }
             
           html += '</div>';
         html += '</div>';
-        
 
         return html;
     };
     
-    function successData( data ) {
-        console.log(data);
+    var successData = function ( data ) {
         var html = "";
         html = createHTML( data );
         
