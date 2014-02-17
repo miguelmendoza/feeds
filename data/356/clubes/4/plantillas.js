e573f31d6c94c77f9a34c20144c37dd8694f2f0b
Miguel Mendoza <miguel.mendoza@esmas.net>
2014-02-17 13:05:11 -0600

    Create plantillas.js

---
+++ data/356/clubes/4/plantillas.js
@@ -0,0 +1,107 @@
+convocatedbyteam({
+    "idTorneo": "356",
+    "nameTorneo": "Torneo MX Clausura 2014",
+    "nameWebTorneo": "torneo-mx-clausura-2014",
+    "idTeam": "45",
+    "nameTeam": "Toluca",
+    "nameWebTeam": "toluca",
+    "dataPlayers": [
+        {
+            "idPlayer": "4710",
+            "playera": "1",
+            "namePlayer": "Hugo Gonza&#769;lez",
+            "position": "Portero",
+            "estadisticas": {
+                "JJ": "4",  "MIN": "340", "GR": "0",  "TA": "0",  "TR": "0",  "AT": "0",  "GA": "0",  "A": "0"
+            }
+        }, {
+            "idPlayer": "12649",
+            "playera": "12",
+            "namePlayer": "Carlos Lo&#769;pez",
+            "position": "Portero",
+            "estadisticas": {
+                "JJ": "4",  "MIN": "340", "GR": "0",  "TA": "0",  "TR": "0",  "AT": "0",  "GA": "0",  "A": "0"
+            }
+        }, {
+            "idPlayer": "381",
+            "playera": "23",
+            "namePlayer": "Moise&#769;s Mun&#771;oz",
+            "position": "Portero",
+            "estadisticas": {
+                "JJ": "4",  "MIN": "340", "GR": "0",  "TA": "0",  "TR": "0",  "AT": "0",  "GA": "0",  "A": "0"
+            }
+        }, {
+            "idPlayer": "29315",
+            "playera": "24",
+            "namePlayer": "Luis Pineda",
+            "position": "Portero",
+            "estadisticas": {
+                "JJ": "4",  "MIN": "340", "GR": "0",  "TA": "0",  "TR": "0",  "AT": "0",  "GA": "0",  "A": "0"
+            }
+        }, {
+            "idPlayer": "244",
+            "playera": "2",
+            "namePlayer": "Francisco Javier Rodri&#769;guez",
+            "position": "Defensa",
+            "estadisticas": {
+                "JJ": "4",  "MIN": "340", "GR": "0",  "TA": "0",  "TR": "0",  "AT": "0",  "GA": "0",  "A": "0"
+            }
+        }, {
+            "idPlayer": "1320",
+            "playera": "3",
+            "namePlayer": "Aquivaldo Mosquera",
+            "position": "Defensa",
+            "estadisticas": {
+                "JJ": "4",  "MIN": "340", "GR": "0",  "TA": "0",  "TR": "0",  "AT": "0",  "GA": "0",  "A": "0"
+            }
+        }, {
+            "idPlayer": "14525",
+            "playera": "4",
+            "namePlayer": "Erik Pimentel",
+            "position": "Defensa",
+            "estadisticas": {
+                "JJ": "4",  "MIN": "340", "GR": "0",  "TA": "0",  "TR": "0",  "AT": "0",  "GA": "0",  "A": "0"
+            }
+        }, {
+            "idPlayer": "2185",
+            "playera": "5",
+            "namePlayer": "Jesús Antonio Molina",
+            "position": "Mediocampista",
+            "estadisticas": {
+                "JJ": "4",  "MIN": "340", "GR": "0",  "TA": "0",  "TR": "0",  "AT": "0",  "GA": "0",  "A": "0"
+            }
+        }, {
+            "idPlayer": "213",
+            "playera": "6",
+            "namePlayer": "Juan Carlos Valenzuela",
+            "position": "Defensa",
+            "estadisticas": {
+                "JJ": "4",  "MIN": "340", "GR": "0",  "TA": "0",  "TR": "0",  "AT": "0",  "GA": "0",  "A": "0"
+            }
+        }, {
+            "idPlayer": "3091",
+            "playera": "7",
+            "namePlayer": "Andrés Ríos",
+            "position": "Delantero",
+            "estadisticas": {
+                "JJ": "4",  "MIN": "340", "GR": "0",  "TA": "0",  "TR": "0",  "AT": "0",  "GA": "0",  "A": "0"
+            }
+        }, {
+            "idPlayer": "29407",
+            "playera": "8",
+            "namePlayer": "Andrés Felipe Andrade",
+            "position": "Mediocampista",
+            "estadisticas": {
+                "JJ": "4",  "MIN": "340", "GR": "0",  "TA": "0",  "TR": "0",  "AT": "0",  "GA": "0",  "A": "0"
+            }
+        }, {
+            "idPlayer": "14811",
+            "playera": "9",
+            "namePlayer": "Raúl Alonso Jiménez",
+            "position": "Delantero",
+            "estadisticas": {
+                "JJ": "4",  "MIN": "340", "GR": "0",  "TA": "0",  "TR": "0",  "AT": "0",  "GA": "0",  "A": "0"
+            }
+        }        
+    ]
+});
